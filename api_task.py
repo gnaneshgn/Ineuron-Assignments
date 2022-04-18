6e6d60df181730b0cbfaf399c1d480ff18543e28
gnaneshgn <45457269+gnaneshgn@users.noreply.github.com>
2022-04-18 19:02:52 +0530

    Add files via upload

---
+++ api_task.py
@@ -0,0 +1,52 @@
+from flask import Flask,request,jsonify
+import psycopg2
+import pymongo
+import json
+from bson import ObjectId
+
+app=Flask(__name__)
+
+
+@app.route('/sqldata',methods=['POST','GET'])
+def fetchSqlData():
+    """
+    returns data from postgresql
+    :return: json type of data
+    """
+    data_list=[]
+    if request.method=='POST':
+        conn=psycopg2.connect(database="lamas",host="localhost",user="postgres",password="Postgresql@009")
+        cur=conn.cursor()
+        cur.execute("Select * from email_registration")
+        db_version = cur.fetchall()
+        for i in db_version:
+            data_list.append(i[1])
+        return jsonify(data_list)
+
+
+@app.route('/mongodata',methods=['POST'])
+def fetchMongoData():
+    """
+    returns data from mongoDb data base
+    :return: json type of data
+    """
+    mongo_data=[]
+    if request.method=='POST':
+        client = pymongo.MongoClient(
+            "mongodb+srv://mongodb:mongodb@cluster0.fvpmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        db = client.test1
+        cursor = db.ineuron_db.find()
+        for i in cursor:
+            i['_id'] = str(i['_id'])
+            mongo_data.append(i)
+        return jsonify(mongo_data)
+
+
+
+
+
+
+
+if __name__=='__main__':
+
+    app.run(debug=True)
